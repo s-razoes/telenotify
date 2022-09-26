b987797f01baa987b5925ea3e8e81c3e2058f02e
s.razoes+github@gmail.com <s-razoes>
2022-09-26 11:45:31 +0100

    propper question module

--- telenotify/question_user.py
+++ telenotify/question_user.py
@@ -1,14 +1,17 @@
-from telenotify.user_interaction import user_interaction
-import sys
+from telenotify import user_interaction
 
-if len(sys.argv) == 1:
-    print('Question for user is mandatory.')
-    exit(1)
+def question(argument,bot_name=None):
+    return user_interaction.question(prompt=argument,bot_name=bot_name, flush=True)
 
-argument = sys.argv[1]
-bot_name=None
-if len(sys.argv) == 3:
-    bot_name = sys.argv[2]
-response = user_interaction.question(argument,bot_name)
-print(response)
-exit(0)
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print('Question for user is mandatory.')
+        exit(1)
+    argument = sys.argv[1]
+    bot_name=None
+    if len(sys.argv) == 3:
+        bot_name = sys.argv[2]
+    response = question(argument,bot_name)
+    print(response)
+    exit(0)
