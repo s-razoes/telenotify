bc89fa4a7373aefc55bd36ca38733f76f1b823f9
s.razoes+github@gmail.com <s-razoes>
2022-10-07 15:16:44 +0100

    Direct bash question with bot name

--- telenotify/question_user.py
+++ telenotify/question_user.py
@@ -6,12 +6,15 @@ def question(argument,bot_name=None):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print('Question for user is mandatory.')
+        print(f'Question for user is mandatory.\nUsage: {sys.argv[0]} BOT_NAME QUESTION\nOr for default bot: {sys.argv[0]} QUESTION')
         exit(1)
-    argument = sys.argv[1]
+    
     bot_name=None
     if len(sys.argv) == 3:
-        bot_name = sys.argv[2]
+        bot_name = sys.argv[1]
+        argument = sys.argv[2]
+    else:
+        argument = sys.argv[1]
     response = question(argument,bot_name)
     print(response)
     exit(0)
